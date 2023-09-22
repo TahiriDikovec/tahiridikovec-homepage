5b4a6bf9f97196e26c899367ec97c2f49c3f05bd
TahiriDikovec <112982918+TahiriDikovec@users.noreply.github.com>
2023-09-22 13:14:10 -0500

    Actualice el main.js.

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Pentester & Social Engineer" />
+        <meta name="author" content="Tahiri Dikovec" />
+        <meta name="author" content="Tahiri Dikovec" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon-v2.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Tahiri Dikovec" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:site" content="@tahiridikovec" />
+        <meta name="twitter:creator" content="@tahiridikovec" />
+        <meta name="twitter:image" content="https://tahiridikovec.com/card.png" />
+        <meta property="og:site_name" content="Tahiri Dikovec" />
+        <meta name="og:title" content="Tahiri Dikovec" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://tahiridikovec.com/card.png" />
         <title>Tahiri Dikovec - Pentester & Social Engineer</title>
       </Head>
 
